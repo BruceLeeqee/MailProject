6e5737b4552d0002ff3431d4b0ffc64c60460077
Jack <305086996@qq.com>
2020-07-11 22:06:22 +0800

    小米商城项目  api json格式

--- mall-api/src/main/java/cn/enjoy/mall/service/IPayService.java
+++ mall-api/src/main/java/cn/enjoy/mall/service/IPayService.java
@@ -1,5 +1,6 @@
 package cn.enjoy.mall.service;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +21,7 @@ public interface IPayService {
     @RequestMapping(value = "/queryByPrepayId", method = RequestMethod.POST)
     String queryByPrepayId(@RequestParam("prepayId") String prepayId) ;
 
-    @RequestMapping(value = "/doPay", method = RequestMethod.POST)
+    @RequestMapping(value = "/doPay", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
     String doPay(@RequestParam("orderId") Integer orderId, @RequestParam("payCode") String payCode,
                  @RequestParam("payAmount") BigDecimal payAmount, @RequestParam("userId") String userId) ;
 }
